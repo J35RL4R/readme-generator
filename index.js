8dc68b7b93487db8e6cfc1715e576c15294ced8a
Jesse R. Larsen <larsenj732@gmail.com>
2020-10-26 20:50:39 -0400

    adding inital draft of code

--- index.js
+++ index.js
@@ -1,16 +1,85 @@
-// array of questions for user
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 const questions = [
+    {
+        type: "input",
+        message: "Enter GitHub username.",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter email address.",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter project's title.",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a short description of your project.",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "Choose which license should your project.",
+        name: "license",
+        choices: [
+            "MIT",
+            "Unlicense",
+            "Apache 2.0",
+            "GNU v3",
+            "BSD 3-Clause",
+            "Mozilla Public License 2.0"
+        ]
+    },
+    {
+        type: "input",
+        message: "Enter any possible install dependencies.",
+        name: "installation",
+        default: "npm install"
+    },
+    {
+        type: "input",
+        message: "What command(s) are needed to run tests.",
+        name: "tests",
+        default: "npm run test"
+    },
+    {
+        type: "input",
+        message: "What do user's need to know about the repository?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "How should users contribute to the repository?",
+        name: "contribute"
+    },
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
+]
+const init = async () => {
+    try {
+        console.log("Readme generator running. \n Please answer the following questions:")
+        const answers = await promptUser();
+        const fileContent = generateMd(answers);
+        await writeToFile("./output/README.md", fileContent);
+        console.log("README.md created.");
+    } 
+    catch (err) {
+        console.error("Error");
+        console.log(err);
+    }
+}
+const promptUser = () => {
+    return inquirer
+        .prompt(questions);
 }
 
-// function to initialize program
-function init() {
-
+const writeToFile = (fileName, data) => {
+    return writeFileAsync(fileName, data);
 }
-
-// function call to initialize program
 init();
